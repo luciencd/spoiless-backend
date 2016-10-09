dac767a0bbbb9452bada76e087b627b3f3c6297c
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2016-10-09 19:07:45 -0400

    added rest, controller, and updater

---
+++ REST/thetvdbupdater.py
@@ -0,0 +1,14 @@
+##here we accept requests from front end for data not dependent on any state of the user
+
+
+##return list of tv shows here:
+
+
+def getShows():
+
+    ##get data from the database for all shows.
+    ##cache the request every day.
+
+
+    ##return well formed list of shows for extension to display.
+    return shows
